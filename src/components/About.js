51f328e12624bf18b57d274ac93d0039bc0b67f7
Vinh <vinhbuith32412000@gmail.com>
2022-07-10 10:46:18 +0700

    Add skill section to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,14 @@
 import { motion } from "framer-motion";
 import { HoverLink } from "./HoverLink";
 
+import HTMLIcon from "../images/html.png";
+import CSSIcon from "../images/css-3.png";
+import JSIcon from "../images/java-script.png";
+import RubyIcon from "../images/ruby.png";
+import RailsIcon from "../images/rails.png";
+import ReactIcon from "../images/react-icon-15.png";
+import GitIcon from "../images/git-icon.png"
+
 const About = () => {
   return(
     <motion.div
@@ -16,8 +24,60 @@ const About = () => {
         <p>I am a <span className="purple-text">Full-Stack</span> developer</p>
       </div>
 
-      <div className="about-illustrate">
+      <div className="about-skills">
+        <h1 className="purple-text">
+          My skills
+        </h1>
+        <h2>
+          Front-end
+        </h2>
+        <div className="skills-container">
+          <div className="skill-container">
+            <h3>HTML</h3>
+            <img className="skill-icon" src={HTMLIcon} alt=""/>
+          </div>
 
+          <div className="skill-container">
+            <h3>CSS</h3>
+            <img className="skill-icon" src={CSSIcon} alt=""/>
+          </div>
+
+          <div className="skill-container">
+            <h3>JavaScript</h3>
+            <img className="skill-icon" src={JSIcon} alt=""/>
+          </div>
+
+          <div className="skill-container">
+            <h3>React</h3>
+            <img className="skill-icon" src={ReactIcon} alt=""/>
+          </div>
+        </div>
+        <h2>
+          Back-end
+        </h2>
+
+        <div className="skills-container">
+          <div className="skill-container">
+            <h3>Ruby</h3>
+            <img className="skill-icon" src={RubyIcon} alt=""/>
+          </div>
+
+          <div className="skill-container">
+            <h3>Rails</h3>
+            <img className="skill-icon" src={RailsIcon} alt=""/>
+          </div>
+        </div>
+
+        <h2>
+          Other
+        </h2>
+
+        <div className="skills-container">
+          <div className="skill-container">
+            <h3>Git</h3>
+            <img className="skill-icon" src={GitIcon} alt=""/>
+          </div>
+        </div>
       </div>
 
       <div className="about-main">
