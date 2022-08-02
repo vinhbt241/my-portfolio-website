36a503d0461254853c54e2a261be39bfed6f3885
Vinh <vinhbuith32412000@gmail.com>
2022-08-02 17:16:33 +0700

    Add more project to work

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,7 +4,8 @@ import { HoverLink } from "./HoverLink";
 
 import ShoppingCartImg from "../images/shopping-cart.png";
 import WaldoImg from "../images/where-is-waldo.png";
-import SocialNetwork from "../images/social_network.jpg"
+import SocialNetworkImg from "../images/social_network.jpg";
+import BattleShipImg from "../images/battle-ship.png"
 
 const Work = () => {
   return(
@@ -18,10 +19,17 @@ const Work = () => {
 
       <div className="project-links">
         <ProjectLink 
+          projectName="Online Battle Ship" 
+          liveLink="https://github.com/vinhbt241/js-battleship"
+          codeLink="https://vinhbt241.github.io/js-battleship/"
+          description="A classic game created with vanilla JavaScript"
+          imgSrc={BattleShipImg} />
+
+        <ProjectLink 
           projectName="Super Hardcore shop" 
           liveLink="https://vinhbt241.github.io/shopping-cart/"
           codeLink="https://github.com/vinhbt241/shopping-cart"
-          description="A shopping UI create by React"
+          description="A shopping UI created using React"
           imgSrc={ShoppingCartImg} />
         <ProjectLink
           projectName="Where is Waldo?"
@@ -35,7 +43,7 @@ const Work = () => {
           liveLink="https://vinhbt241.github.io/js-social-network/"
           codeLink="https://github.com/vinhbt241/js-social-network"
           description="A social network built with React and Rails"
-          imgSrc={SocialNetwork}
+          imgSrc={SocialNetworkImg}
           />
       </div>
     </motion.div>
